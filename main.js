b33b6e75c789929478045abcb11fa92145a4fa14
Tony Fazio <antoniojfazio@gmail.com>
2014-04-29 09:34:04 -0500

    FInished tic-tac-toe

--- main.js
+++ main.js
@@ -28,11 +28,17 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
-    // TODO: Check for rest of game winning cases
+
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
   )
   {
     console.log('somebody won');
     // TODO: Trigger 'game-win' event with the winning player as the event data
+    $(document).trigger('game-win');
   }
 };
 
@@ -42,17 +48,21 @@ $(document).on('click', '#board .space', function (e) {
 
   // Marks the space with the current player's name
   // TODO: Don't mark it unless the space is blank
-  spaces[spaceNum] = currentPlayer;
+  if (spaces[spaceNum]){
+     alert("That space is already taken! Choose another!");
+  }
+  else{
+    spaces[spaceNum] = currentPlayer;
   // Adds a class to elem so css can take care of the visuals
-  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
-
-  checkForWinner();
-  setNextTurn();
+    $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+    checkForWinner();
+    setNextTurn();
+  }
 });
 
 $(document).on('game-win', function (e, winner) {
   // TODO: Alert who won the game
-  alert(winner);
+  alert(currentPlayer + " won the game!");
 });
 
 // Start the game
